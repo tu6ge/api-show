57201c7751e65c63aedadb6d5d90ce014a8160ef
tu6ge <772364230@qq.com>
2019-10-16 22:21:56 +0800

    左侧菜单优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,29 +6,30 @@ module.exports = {
           { text: '首页', link: '/' },
           { text: '指南', link: '/guide/' },
         ],
+        sidebarDepth:1,
+        
         sidebar: [
-            '/guide/',
-            '/guide/jisutvjmyg', //电视节目预告
-            '/guide/ipquery',       //ip地址归属地查询
-            '/guide/shares',       //股票行情
-            '/guide/pdp-elite',       //性格分析
-            '/guide/ocr-businesslicense',       //营业执照图像识别
-            '/guide/ai-guidance-elite',       //AI智能导诊
-            '/guide/ai-driving-vehicle-license',       //驾驶证识别
-            '/guide/todayoil',       //今日油价
-            '/guide/gold',       //黄金价格查询
-            '/guide/finance-calendar',       //财经日历
-            '/guide/waihui',       //外汇牌价汇率
-            '/guide/bankcard',       //银行卡归属地查询
-            { 
-                title:'分组一',
+            '/guide/',{
+                title:'接口列表',
+                collapsable: false,
                 children:[
-                    ['/hello','Hello'],
+                    ['/guide/jisutvjmyg','电视节目预告'], //电视节目预告
+                    '/guide/ipquery',       //ip地址归属地查询
+                    '/guide/shares',       //股票行情
+                    '/guide/pdp-elite',       //性格分析
+                    '/guide/ocr-businesslicense',       //营业执照图像识别
+                    '/guide/ai-guidance-elite',       //AI智能导诊
+                    '/guide/ai-driving-vehicle-license',       //驾驶证识别
+                    '/guide/todayoil',       //今日油价
+                    '/guide/gold',       //黄金价格查询
+                    '/guide/finance-calendar',       //财经日历
+                    '/guide/waihui',       //外汇牌价汇率
+                    '/guide/bankcard',       //银行卡归属地查询
                 ]
-                
             }
+            
         ],
-        displayAllHeaders: true,
+        displayAllHeaders: false,
         lastUpdated: '最后更新时间',
         repo:'tu6ge/api-show',
         docsDir: 'docs',
