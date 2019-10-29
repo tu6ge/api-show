e11fe4a2adacb54364a954fb3cbefaa010c3a612
tu6ge <772364230@qq.com>
2019-10-29 14:27:33 +0800

    test lint-staged

--- packages/ali_033200/src/query.vue
+++ packages/ali_033200/src/query.vue
@@ -1,21 +1,17 @@
 <template>
-    <div >
-        
-    </div>
+  <div></div>
 </template>
 <script>
-
 export default {
-    name: "aps-ali-033200",
-    props:{
-        data:{
-            type:  Object,
-            default: function () {
-                return { }
-            }
-        },
-    },
-    components:{
+  name: "aps-ali-033200",
+  props: {
+    data: {
+      type: Object,
+      default: function() {
+        return {};
+      }
     }
-}
+  },
+  components: {}
+};
 </script>
