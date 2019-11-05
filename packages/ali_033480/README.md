8d52649ade2f5837f3f0ab56012348d11966f15e
tu6ge <772364230@qq.com>
2019-11-05 16:29:44 +0800

    实时快递查询

---
+++ packages/ali_033480/README.md
@@ -0,0 +1,7 @@
+
+# 实时快递物流查询
+
+## 阿里云地址
+> https://market.aliyun.com/products/57126001/cmapi033480.html
+
+## 使用方法
---
+++ packages/ali_033480/src/query.vue
@@ -0,0 +1,21 @@
+<template>
+    <div >
+        
+    </div>
+</template>
+<script>
+
+export default {
+    name: "aps-ali-033480",
+    props:{
+        data:{
+            type:  Object,
+            default: function () {
+                return { }
+            }
+        },
+    },
+    components:{
+    }
+}
+</script>
