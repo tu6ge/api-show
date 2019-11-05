7349fca49212ee265eed2dfce1e495824ea56bdc
tu6ge <772364230@qq.com>
2019-11-05 21:00:05 +0800

    快递查询

--- packages/ali_026963/src/query.vue
+++ packages/ali_026963/src/query.vue
@@ -1,21 +1,47 @@
 <template>
-    <div >
-        
-    </div>
+  <a-card :title="'快递单号：' + data.no">
+    <a :href="site" slot="extra" target="_blank">
+      {{ data.name }}
+    </a>
+    <van-steps direction="vertical" :active="0" :active-color="color">
+      <van-step v-for="(item, key) in list" :key="key">
+        <h3>{{ item.content }}</h3>
+        <p>{{ item.time }}</p>
+      </van-step>
+    </van-steps>
+  </a-card>
 </template>
 <script>
-
+import { Steps, Step } from "vant";
+import "vant/lib/steps/style";
+import "vant/lib/step/style";
+import { Card } from "ant-design-vue";
+import "ant-design-vue/lib/card/style/css";
 export default {
-    name: "aps-ali-026963",
-    props:{
-        data:{
-            type:  Object,
-            default: function () {
-                return { }
-            }
-        },
-    },
-    components:{
+  name: "aps-ali-026963",
+  props: {
+    data: {
+      type: Object,
+      default: function() {
+        return {};
+      }
     }
-}
+  },
+  components: {
+    [Step.name]: Step,
+    [Steps.name]: Steps,
+    [Card.name]: Card
+  },
+  computed: {
+    list() {
+      return this.data.list;
+    },
+    color() {
+      return this.data.state == 3 ? "#07c160" : "#ff7800";
+    },
+    site() {
+      return "http://" + this.data.site;
+    }
+  }
+};
 </script>
