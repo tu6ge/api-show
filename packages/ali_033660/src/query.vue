187f466eecf335aac664f21afe88f7705066d4fb
tu6ge <772364230@qq.com>
2019-11-06 17:22:29 +0800

    公交查询

---
+++ packages/ali_033660/src/query.vue
@@ -0,0 +1,21 @@
+<template>
+    <div >
+        
+    </div>
+</template>
+<script>
+
+export default {
+    name: "aps-ali-033660",
+    props:{
+        data:{
+            type:  Object,
+            default: function () {
+                return { }
+            }
+        },
+    },
+    components:{
+    }
+}
+</script>
